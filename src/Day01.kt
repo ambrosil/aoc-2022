f993b540e5309f12a819e65e55813ab5ebded64d
Luca Ambrosi <lamb.cons@bene.it>
2022-12-01 11:26:40 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,20 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var elfNum = 0
+        return input.groupBy { if (it.isBlank()) elfNum++; elfNum }
+                    .map { elf -> elf.value.filterNot { calories -> calories.isBlank() } }
+                    .maxOf { it.sumOf { n -> n.toInt() } }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var elfNum = 0
+        return input.groupBy { if (it.isBlank()) elfNum++; elfNum }
+            .map { elf -> elf.value.filterNot { calories -> calories.isBlank() } }
+            .map { it.sumOf { n -> n.toInt() } }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
