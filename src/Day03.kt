6233dbaf392f5df8128abc11d8c17742ce0efb1d
Luca Ambrosi <luca@luca.it>
2022-12-03 18:16:32 +0100

    Day03

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,11 +1,37 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return 0
+
+    fun Set<Char>.points(): Int {
+        val c = single()
+        return if (c.isLowerCase()) {
+            1 + (c - 'a')
+        } else {
+            27 + (c - 'A')
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun String.halve(): List<String> {
+        val half = length / 2
+        return listOf(substring(0, half), substring(half))
     }
+
+    fun part1(input: List<String>) =
+        input.sumOf { item ->
+            val (first, second) = item.halve()
+            first.toSet()
+                .intersect(second.toSet())
+                .points()
+        }
+
+    fun part2(input: List<String>) =
+        input.chunked(3)
+            .map {
+                it.fold(it[0].toSet()) { acc, s ->
+                    acc.intersect(s.toSet())
+                }
+            }
+            .sumOf {
+                it.points()
+            }
 
     val input = readInput("Day03")
     println(part1(input))
