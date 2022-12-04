878cd9c570a7ea18640973dab2f8c7d01aaa8595
Luca Ambrosi <luca@luca.it>
2022-12-04 14:50:05 +0100

    Day04

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,14 +1,32 @@
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        return 0
-    }
+    fun part1(input: List<String>) =
+        input
+            .readRanges()
+            .count { (first, second) ->
+                first contains second || second contains first
+            }
 
-    fun part2(input: List<String>): Int {
-        return 0
-    }
+    fun part2(input: List<String>) =
+        input
+            .readRanges()
+            .count { (first, second) ->
+                first overlaps second
+            }
 
     val input = readInput("Day04")
     println(part1(input))
     println(part2(input))
 }
+
+fun List<String>.readRanges() =
+    this.map {
+        it.split(",").map { s ->
+            val (start, end) = s.split("-")
+            start.toInt()..end.toInt()
+        }
+    }
+
+infix fun IntRange.contains(second: IntRange) = this.first <= second.first && this.last >= second.last
+infix fun IntRange.overlaps(second: IntRange) = (this intersect second).isNotEmpty()
+
